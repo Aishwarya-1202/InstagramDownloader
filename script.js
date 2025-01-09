79c4cde405a941617dc0f03b54d824047ad6a202
Aishwarya-1202 <chenna.aishwarya2602@gmail.com>
2025-01-09 14:39:37 +0530

    Added Styling and JavaScript

---
+++ script.js
@@ -0,0 +1,63 @@
+let search=document.querySelector('.search');
+let download=document.querySelector('.download');
+
+download.addEventListener('click',()=>{
+    let url=search.value;
+    let shortcode="";
+    for(let i=31;i<42;i++){
+        shortcode=shortcode+url[i];
+    }
+    console.log(shortcode);
+    let link= `https://instagram-scraper-api2.p.rapidapi.com/v1/post_info?code_or_id_or_url=${shortcode}&include_insights=true`;
+    getVideo(link);
+});
+
+let getVideo=async (url)=>{
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e',
+            'x-rapidapi-host': 'instagram-scraper-api2.p.rapidapi.com'
+        }
+    };
+    try {
+	    const response = await fetch(url, options);
+	    const result = await response.json();
+        console.log(result);
+	    const videoElement = document.getElementById('myVid');
+        let videoSRC=result.data.video_url;
+        videoElement.src = videoSRC;
+        const msg=document.querySelector('.msg');
+        if(videoSRC!=""){
+        msg.style.display="block";
+        }
+    } catch (error) {
+	    console.error(error);
+        console.log("Calling 2nd API");
+        getVideo2(search.value);
+    }
+}
+let getVideo2=async(link)=>{
+    const url = `https://instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com/get-info-rapidapi?url=${link}`;
+    const options = {
+    	method: 'GET',
+	    headers: {
+		    'x-rapidapi-key': '5bae2b20b3mshfa5923da98afd77p138521jsnce7c2c61fc0e',
+		    'x-rapidapi-host': 'instagram-downloader-download-instagram-videos-stories1.p.rapidapi.com'
+	    }
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+	    const videoElement = document.getElementById('myVid');
+        let videoSRC=result.download_url;
+        videoElement.src = videoSRC;
+        const msg=document.querySelector('.msg');
+        if(videoSRC!=""){
+        msg.style.display="block";
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
